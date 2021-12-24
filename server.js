ebf49c7b00022627893c696f99c81eca4ee40e62
bagus1986 <bagus100486@gmail.com>
2021-12-24 13:54:25 +0700

    setting reutes di server js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use[bodyParser.urlencoded({extended:true})];
 app.use(bodyParser.json());
 
+//panggil routes koneksi mysql
+var routes = require('./routes');
+routes(app);
+
 app.listen(3000, () => {
     console.log('server started on port');
 });
